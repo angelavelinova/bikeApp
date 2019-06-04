77d9d04a403d22a3ce26f9a2b67a7e096fc7aa28
angelavelinova <43788986+angelavelinova@users.noreply.github.com>
2019-06-04 10:45:59 +0300

    Update models.py

--- bike/models.py
+++ bike/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Bike(models.Model):
-    model = models.CharField(max_length=256)
-    seats = models.IntegerField()
+    reserved = models.IntegerField()#0-ne e rezervirano(t.e. e svobodno), 
+    #1 - rezervirano e, no ne ni interesuva ot kogo, 2- zaeto e
+    current_owner = models.CharField(max_length=256)# moje null, ako reserved = 0 
+  
